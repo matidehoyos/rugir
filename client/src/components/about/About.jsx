ee364f7ee4af24532bf4d3b5ff81177ace74d23c
Matias <mati.dehoyosmdp@gmail.com>
2024-06-20 20:49:49 -0300

    }

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -3,7 +3,7 @@ import style from './About.module.css'
 function About() {
   return (
     <div className={style.container} id='about'>
-            <img className={style.image} src="./galgo.png" alt="logo rugir" />
+            <div className={style.image}></div>
             <div className={style.caja}>
                 <h3>Sobre nosotros</h3>
                 <p><span>RUGIR</span> es un emprendimiento marplatense, que comenzo como un hobby, y de apoco, y gracias a ustedes pudo ir creciendo hasta convertirse hoy en día, en nuestra fuente de trabajo.<br/> Estamos felices, que atraves de nuestros productos, nuestros clientes puedan trasmitir a su ser querido u a esa persona tan especial, su cariño y eligiendonos como su regalo a entregar.</p>
