561a046f40a6080bd88670499a6def532e9b4a7b
Matias <mati.dehoyosmdp@gmail.com>
2024-06-21 18:33:04 -0300

    client

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -7,7 +7,7 @@ function About() {
             <div className={style.imag}></div>
             <div className={style.caja}>
                 <h3>Sobre nosotros</h3>
-                <p><span>RUGIR</span> es un emprendimiento marplatense, que comenzo como un hobby, y de apoco, y gracias a ustedes pudo ir creciendo hasta convertirse hoy en día, en nuestra fuente de trabajo.<br/> Estamos felices, que atraves de nuestros productos, nuestros clientes puedan trasmitir a su ser querido u a esa persona tan especial, su cariño y eligiendonos como su regalo a entregar.</p>
+                <p><span>RUGIR</span> es un emprendimiento marplatense, que nace como un hobby, y de apoco, y gracias a ustedes pudo ir creciendo hasta convertirse hoy en día, en nuestra profesión.<br/> Somos una familia, y estamos felices de crear productos de primera calidad, y que nos elijan a la hora de decorar una casa, un quincho, una habitación o ser un regalo muy especial para alguien.</p>
             </div>
     </div>
   )
