844c36406bddcbc8e12e19f220d3051f0f14fb03
Matias <mati.dehoyosmdp@gmail.com>
2024-06-21 18:26:34 -0300

    {

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Contact from './components/contact/Contact'
 import Encargo from './components/encargo/Encargo'
 import Footer from './components/footer/Footer'
 import Header from './components/header/Header'
+import Intro from './components/intro/Intro'
 import NavBar from './components/navBar/NavBar'
 import Ubicacion from './components/ubicacion/Ubicacion'
 
@@ -14,6 +15,7 @@ function App() {
 
   return (
     <>
+      <Intro />
       <NavBar />
       <Header />
       <Encargo />
